a62d174226d13234ad5c91559910e6960fc7beec
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-08 19:13:28 +0200

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -7,6 +7,9 @@ import styled from 'styled-components'
 const StyledFriend = styled.div`
   color: red;
   font-weight: bold;
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
 
   &:hover {
     color: green;
@@ -15,7 +18,7 @@ const StyledFriend = styled.div`
 
 export default function Friend({ info, action }) {
   return (
-    <StyledFriend className='friend'>
+    <StyledFriend>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
