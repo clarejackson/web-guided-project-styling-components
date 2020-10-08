30ccc21575b3b7116db4863d3d996ec7a98016c0
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-08 18:53:00 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { }
+import { ThemeProvider } from 'styled-components'
 import App from './components/App'
 
 import './styles.css'
