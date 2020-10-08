db908c873b2f7858374e0657f52d97c89800e77f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-08 19:03:27 +0200

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -2,18 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 // OUTSIDE OF THE FRIEND COMPONENT
-// 
+// CAPITALIZE THE SC
+// DON'T FORGET YOUR SEMICOLONS
 const StyledFriend = styled.div`
   color: red;
 `
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend className='friend'>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
